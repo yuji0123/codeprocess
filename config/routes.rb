9d61925278d96080537150e88c68375f9a482b87
Yuji Koketsu <kyuji7123.public@gmail.com>
2015-07-12 13:15:35 +0900

    当て込み３

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :ranks
+  match "steps/sendStep", :controller => :steps, :acion => :sendStep, :via => :POST
   resources :steps
   resources :articles
   resources :users
   root to: 'toppage#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
